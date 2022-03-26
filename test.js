dda0b09307796e93799060aa7bf3c5df13679b29
Maria Mercedes Piguillem <mercedes.piguillem@gmail.com>
2022-03-26 11:30:20 -0300

    Desafio clase 4 de coderhouse

---
+++ test.js
@@ -0,0 +1,24 @@
+const Contenedor = require("./contenedor.js");
+
+const newProduct1 = {
+  title: "Musculosa",
+  price: 700,
+  thumbnail:
+    "https://www.google.com/search?q=remera&rlz=1C1PDZP_esAR950AR952&sxsrf=APq-WBvGJ0yfEGv02qvGoxdiEgrAwpMPIQ:1648269028651&source=lnms&tbm=isch&sa=X&ved=2ahUKEwictcnP-OL2AhUAGbkGHZRqDn4Q_AUoAXoECAEQAw&biw=1366&bih=625&dpr=1#imgrc=ffXK8uDRHLRKYM",
+};
+
+const newProduct2 = {
+  title: "Zapas",
+  price: 1000,
+  thumbnail:
+    "https://www.google.com/search?q=zapatillas&tbm=isch&ved=2ahUKEwj9rr_e-OL2AhVJqJUCHXnwBbQQ2-cCegQIABAA&oq=zapa&gs_lcp=CgNpbWcQARgAMgcIABCxAxBDMgQIABBDMgQIABBDMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMgQIABBDMgQIABBDMgcIABCxAxBDMgcIABCxAxBDOgcIIxDvAxAnOgsIABCABBCxAxCDAToICAAQgAQQsQM6CAgAELEDEIMBOgUIABCABDoKCCMQ7wMQ6gIQJ1DhBliCEGDEGmgBcAB4AIABW4gB_QOSAQE2mAEAoAEBqgELZ3dzLXdpei1pbWewAQrAAQE&sclient=img&ei=A5c-Yr2hOcnQ1sQP-eCXoAs&bih=625&biw=1366&rlz=1C1PDZP_esAR950AR952#imgrc=znrGW4H-knEarM",
+};
+
+const contenedorTest = new Contenedor("./products.txt");
+
+//prueba.save(product1).then(x=>console.log(x))
+//prueba.save(product2).then(x=>console.log(x))
+// prueba.save(product3).then(x=>console.log(x))
+//prueba.getById(3).then(x => console.log(x))
+contenedorTest.deleteById(5);
+// contenedorTest.deleteAll();
